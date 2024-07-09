e4102d772566f4ce646c51a3682fb69bb7dc297b
andifahruddinakas <andifahruddinakas@users.noreply.github.com>
2024-07-09 05:26:44 +0000

    Blade Prettier Code

--- resources/views/admin/pengaturan_surat/partials/pengaturan_tanda_tangan.blade.php
+++ resources/views/admin/pengaturan_surat/partials/pengaturan_tanda_tangan.blade.php
@@ -97,39 +97,32 @@
                 </div>
             </div>
         </div>
-        <div id="ttd-scan" @if(setting('tte') == '1') style="display: none;" @endif>
+        <div id="ttd-scan" @if (setting('tte') == '1') style="display: none;" @endif>
             <div class="form-group">
                 <label>TTD Scan</label>
                 <div class="input-group col-xs-12 col-sm-8">
                     <div class="btn-group col-xs-12 col-sm-8" data-toggle="buttons" style="padding: 0px;">
-                        <label
-                            class="btn btn-info btn-flat btn-sm col-xs-6 col-sm-5 col-lg-3 form-check-label @active(setting('ttd_scan') == '1') @disabled(!$kades)">
-                            <input type="radio" name="ttd_scan" class="form-check-input" value="1" autocomplete="off"
-                                @checked(setting('ttd_scan') == '1')>
+                        <label class="btn btn-info btn-flat btn-sm col-xs-6 col-sm-5 col-lg-3 form-check-label @active(setting('ttd_scan') == '1') @disabled(!$kades)">
+                            <input type="radio" name="ttd_scan" class="form-check-input" value="1" autocomplete="off" @checked(setting('ttd_scan') == '1')>
                             Ya</label>
-                        <label
-                            class="btn btn-info btn-flat btn-sm col-xs-6 col-sm-5 col-lg-3 form-check-label @active(setting('ttd_scan') == '0' || setting('ttd_scan') == null) @disabled(!$kades)">
-                            <input type="radio" name="ttd_scan" class="form-check-input" value="0" autocomplete="off"
-                                @checked(setting('ttd_scan') == '0' || setting('ttd_scan') == null)>
+                        <label class="btn btn-info btn-flat btn-sm col-xs-6 col-sm-5 col-lg-3 form-check-label @active(setting('ttd_scan') == '0' || setting('ttd_scan') == null) @disabled(!$kades)">
+                            <input type="radio" name="ttd_scan" class="form-check-input" value="0" autocomplete="off" @checked(setting('ttd_scan') == '0' || setting('ttd_scan') == null)>
                             Tidak
                         </label>
                     </div>
                 </div>
             </div>
-            <div id="visual-ttd-form" @if(setting('ttd_scan') == '0' || setting('ttd_scan') == null) style="display: none;" @endif>
+            <div id="visual-ttd-form" @if (setting('ttd_scan') == '0' || setting('ttd_scan') == null) style="display: none;" @endif>
                 <div class="form-group">
                     <label>Visual</label>
                     <div class="input-group input-group-sm col-md-3 col-sm-12">
-                        <img class="img-responsive"
-                            src="{{ setting('visual_ttd_scan') == null ? asset('assets/images/ttd_scan.png?v', false) : base_url(setting('visual_ttd_scan')) }}"
-                            alt="TTD Scan">
+                        <img class="img-responsive" src="{{ setting('visual_ttd_scan') == null ? asset('assets/images/ttd_scan.png?v', false) : base_url(setting('visual_ttd_scan')) }}" alt="TTD Scan">
                     </div>
                     <div class="input-group input-group-sm col-md-3 col-sm-12">
                         <input type="text" class="form-control" id="file_path1">
                         <input type="file" id="file1" class="hidden" name="visual_ttd_scan" accept=".jpg,.jpeg,.png">
                         <span class="input-group-btn">
-                            <button type="button" class="btn btn-info btn-flat" id="file_browser1"><i
-                                    class="fa fa-search"></i></button>
+                            <button type="button" class="btn btn-info btn-flat" id="file_browser1"><i class="fa fa-search"></i></button>
                         </span>
                     </div>
                 </div>
@@ -137,16 +130,14 @@
                     <div class="col-sm-6">
                         <div class="input-group" style="margin-top: 3px; margin-bottom: 3px">
                             <span class="input-group-addon input-sm">Tinggi</span>
-                            <input type="number" class="form-control input-sm required" name="visual_ttd_height"
-                                style="text-align:right;" value="{{ setting('visual_ttd_height') }}">
+                            <input type="number" class="form-control input-sm required" name="visual_ttd_height" style="text-align:right;" value="{{ setting('visual_ttd_height') }}">
                             <span class="input-group-addon input-sm">px</span>
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="input-group" style="margin-top: 3px; margin-bottom: 3px">
                             <span class="input-group-addon input-sm">Lebar</span>
-                            <input type="number" class="form-control input-sm required" name="visual_ttd_width"
-                                style="text-align:right;" value="{{ setting('visual_ttd_width') }}">
+                            <input type="number" class="form-control input-sm required" name="visual_ttd_width" style="text-align:right;" value="{{ setting('visual_ttd_width') }}">
                             <span class="input-group-addon input-sm">px</span>
                         </div>
                     </div>
