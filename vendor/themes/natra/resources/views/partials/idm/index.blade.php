87a143e1889d5956bcf82450af23d2dff5a35db8
Andi Fahruddin Akas <57283157+andifahruddinakas@users.noreply.github.com>
2024-11-28 08:56:02 +0800

    tampilan idm

--- vendor/themes/natra/resources/views/partials/idm/index.blade.php
+++ vendor/themes/natra/resources/views/partials/idm/index.blade.php
@@ -23,113 +23,99 @@
 @endpush
 
 @section('content')
-<div class="content-wrapper">
-	<section class="content-header">
-		<h1>Status Indeks Desa Membangun (IDM) {{ $tahun }}</h1>
-	</section>
-	<section class="content" id="maincontent">
-		<div class="box box-info">
-			<div id="status-error" style="display: none;">
-				<div class="box-body">
-					<div class="alert alert-danger">
-						<p id="error-message"></p>
-					</div>
+<div class="single_page_area">
+	<h2 class="post_titile">Status Indeks Desa Membangun (IDM) {{ $tahun }}</h2>
+	<div class="box box-info">
+		<div id="status-error" style="display: none;">
+			<div class="box-body">
+				<div class="alert alert-danger">
+					<p id="error-message"></p>
 				</div>
 			</div>
+		</div>
 
-			<div id="status-idm" style="display: none;">
-				<div class="box-body">
-					<div class="row">
-						<div class="col-lg-6 col-xs-12">
-							<div class="row">
-								<div class="col-lg-6 col-xs-12">
-									<div class="small-box bg-blue">
-										<div class="inner">
-											<h3 id="skor-saat-ini"></h3>
-											<p>SKOR IDM SAAT INI</p>
-										</div>
-										<div class="icon">
-											<i class="ion ion-stats-bars"></i>
-										</div>
+		<div id="status-idm" style="display: none;">
+			<div class="box-body">
+				<div class="row">
+					<div class="col-lg-6 col-xs-12">
+						<div class="row">
+							<div class="col-lg-6 col-xs-12">
+								<div class="small-box bg-blue">
+									<div class="inner">
+										<h3 id="skor-saat-ini"></h3>
+										<p>SKOR IDM SAAT INI</p>
 									</div>
-								</div>
-								<div class="col-lg-6 col-xs-12">
-									<div class="small-box bg-yellow">
-										<div class="inner">
-											<h3 id="status-saat-ini"></h3>
-											<p>STATUS IDM</p>
-										</div>
-										<div class="icon">
-											<i class="ion-ios-pulse-strong"></i>
-										</div>
-									</div>
-								</div>
-								<div class="col-lg-6 col-xs-12">
-									<div class="small-box bg-red">
-										<div class="inner">
-											<h3 id="skor-minimal"></h3>
-											<p>SKOR IDM MINIMAL</p>
-										</div>
-										<div class="icon">
-											<i class="ion ion-ios-pie"></i>
-										</div>
-									</div>
-								</div>
-								<div class="col-lg-6 col-xs-12">
-									<div class="small-box bg-green">
-										<div class="inner">
-											<h3 id="target-status"></h3>
-											<p>TARGET STATUS</p>
-										</div>
-										<div class="icon">
-											<i class="ion ion-arrow-graph-up-right"></i>
-										</div>
-									</div>
-								</div>
-
-								<div class="col-lg-12 col-xs-12">
-									<div class="table-responsive">
-										<table class="table table-bordered table-striped dataTable table-hover">
-											<tbody>
-												<tr>
-													<td width="30%">PROVINSI</td>
-													<td width="1">:</td>
-													<td id="nama-provinsi"></td>
-												</tr>
-												<tr>
-													<td>KABUPATEN</td>
-													<td> : </td>
-													<td id="nama-kabupaten"></td>
-												</tr>
-												<tr>
-													<td>{{ strtoupper(setting('sebutan_kecamatan')) }}</td>
-													<td> : </td>
-													<td id="nama-kecamatan"></td>
-												</tr>
-												<tr>
-													<td>{{ strtoupper(setting('sebutan_desa')) }}</td>
-													<td> : </td>
-													<td id="nama-desa"></td>
-												</tr>
-											</tbody>
-										</table>
+									<div class="icon">
+										<i class="ion ion-stats-bars"></i>
 									</div>
 								</div>
 							</div>
-						</div>
+							<div class="col-lg-6 col-xs-12">
+								<div class="small-box bg-yellow">
+									<div class="inner">
+										<h3 id="status-saat-ini"></h3>
+										<p>STATUS IDM</p>
+									</div>
+									<div class="icon">
+										<i class="ion-ios-pulse-strong"></i>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6 col-xs-12">
+								<div class="small-box bg-red">
+									<div class="inner">
+										<h3 id="skor-minimal"></h3>
+										<p>SKOR IDM MINIMAL</p>
+									</div>
+									<div class="icon">
+										<i class="ion ion-ios-pie"></i>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6 col-xs-12">
+								<div class="small-box bg-green">
+									<div class="inner">
+										<h3 id="target-status"></h3>
+										<p>TARGET STATUS</p>
+									</div>
+									<div class="icon">
+										<i class="ion ion-arrow-graph-up-right"></i>
+									</div>
+								</div>
+							</div>
 
-						<div class="col-lg-6 col-xs-12">
-							<figure class="highcharts-figure">
-								<div id="container"></div>
-							</figure>
+							<div class="col-lg-12 col-xs-12">
+								<div class="table-responsive">
+									<table class="table table-bordered table-striped dataTable table-hover">
+										<tbody>
+											<tr>
+												<td width="30%">PROVINSI</td>
+												<td width="1">:</td>
+												<td id="nama-provinsi"></td>
+											</tr>
+											<tr>
+												<td>KABUPATEN</td>
+												<td> : </td>
+												<td id="nama-kabupaten"></td>
+											</tr>
+											<tr>
+												<td>{{ strtoupper(setting('sebutan_kecamatan')) }}</td>
+												<td> : </td>
+												<td id="nama-kecamatan"></td>
+											</tr>
+											<tr>
+												<td>{{ strtoupper(setting('sebutan_desa')) }}</td>
+												<td> : </td>
+												<td id="nama-desa"></td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
 						</div>
 					</div>
 
-					<div class="row">
-						<hr>
-					</div>
-
-					<div class="col-md-8">
+					<div class="col-lg-6 col-xs-12">
 						<figure class="highcharts-figure">
 							<div id="container"></div>
 						</figure>
@@ -137,37 +123,47 @@
 				</div>
 
 				<div class="row">
-					<div class="col-sm-12">
-						<div class="table-responsive">
-							<table class="table table-bordered dataTable table-striped table-hover" id="tabel-daftar">
-								<thead class="bg-gray color-palette">
-									<tr>
-										<th rowspan="2" class="padat">NO</th>
-										<th rowspan="2">INDIKATOR IDM</th>
-										<th rowspan="2">SKOR</th>
-										<th rowspan="2">KETERANGAN</th>
-										<th rowspan="2" nowrap>KEGIATAN YANG DAPAT DILAKUKAN</th>
-										<th rowspan="2">+NILAI</th>
-										<th colspan="6" class="text-center">YANG DAPAT MELAKSANAKAN KEGIATAN</th>
-									</tr>
-									<tr>
-										<th>PUSAT</th>
-										<th>PROVINSI</th>
-										<th>KABUPATEN</th>
-										<th>DESA</th>
-										<th>CSR</th>
-										<th>LAINNYA</th>
-									</tr>
-								</thead>
-								<tbody>
-								</tbody>
-							</table>
-						</div>
+					<hr>
+				</div>
+
+				<div class="col-md-8">
+					<figure class="highcharts-figure">
+						<div id="container"></div>
+					</figure>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="table-responsive">
+						<table class="table table-bordered dataTable table-striped table-hover" id="tabel-daftar">
+							<thead class="bg-gray color-palette">
+								<tr>
+									<th rowspan="2" class="padat">NO</th>
+									<th rowspan="2">INDIKATOR IDM</th>
+									<th rowspan="2">SKOR</th>
+									<th rowspan="2">KETERANGAN</th>
+									<th rowspan="2" nowrap>KEGIATAN YANG DAPAT DILAKUKAN</th>
+									<th rowspan="2">+NILAI</th>
+									<th colspan="6" class="text-center">YANG DAPAT MELAKSANAKAN KEGIATAN</th>
+								</tr>
+								<tr>
+									<th>PUSAT</th>
+									<th>PROVINSI</th>
+									<th>KABUPATEN</th>
+									<th>DESA</th>
+									<th>CSR</th>
+									<th>LAINNYA</th>
+								</tr>
+							</thead>
+							<tbody>
+							</tbody>
+						</table>
 					</div>
 				</div>
 			</div>
 		</div>
-	</section>
+	</div>
 </div>
 @endsection
 
