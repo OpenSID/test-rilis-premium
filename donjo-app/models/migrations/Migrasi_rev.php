e9e825868a8f700a1ba11e32f086591d9be2be5f
Andi Fahruddin Akas <57283157+andifahruddinakas@users.noreply.github.com>
2024-12-02 22:11:43 +0800

    Update Migrasi_rev.php

--- donjo-app/models/migrations/Migrasi_rev.php
+++ donjo-app/models/migrations/Migrasi_rev.php
@@ -51,7 +51,7 @@ class Migrasi_rev extends MY_Model
 {
     public function up()
     {
-        return true;
+        $hasil = true;
 
         // Migrasi berdasarkan config_id
         $config_id = DB::table('config')->pluck('id')->toArray();
@@ -83,7 +83,6 @@ class Migrasi_rev extends MY_Model
     // keuangan
     public function migrasi_2024071251($hasil)
     {
-        log_message('error', 'Migrasi keuangan');
         if (! Schema::hasTable('keuangan_template')) {
             Schema::create('keuangan_template', static function (Blueprint $table) {
                 $table->uuid()->primary();
