23c892c7c4f06a14b873657688efe9fd6cbc7ca2
FirlianiF <firlianif@gmail.com>
2025-03-09 10:59:24 +0700

    u

--- app/Listeners/Login2faAdminListener.php
+++ app/Listeners/Login2faAdminListener.php
@@ -37,17 +37,19 @@
 
 namespace App\Listeners;
 
-use App\Models\LogLogin;
-use Exception;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Container\Container;
-use Illuminate\Support\Carbon;
-use NotificationChannels\Telegram\Telegram;
+use App\Libraries\OTP\OtpManager;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class Login2faAdminListener
 {
+    private OtpManager $otp;
+
     public function __construct(protected Container $app)
     {
+        $this->otp = new OtpManager();
     }
 
     public function handle(Login $login): void
@@ -55,8 +57,23 @@ class Login2faAdminListener
         if (! in_array($login->guard, ['admin'])) {
             return;
         }
-//cek tfa aktif atau engga, kalau aktif kirim email otp
 
+        //cek tfa aktif atau tidak, kalau aktif kirim email otp
+        if($login->user->tfa_enabled == 1){
+            $email   = $login->user->email;
+            $token   = hash('sha256', $raw_token = random_int(100000, 999999));
+            $id_user = $login->user->id;
 
+            if ($this->otp->driver('email')->cekAkunTerdaftar(['email' => $email, 'id' => $id_user])) {
+                    // TODO: OpenKab - Perlu disesuaikan ulang setelah semua modul selesai
+                    User::where('id', $id_user)->update([
+                        'email'                => $email,
+                        'email_token'          => $token,
+                        'email_tgl_kadaluarsa' => date('Y-m-d H:i:s', strtotime(date('Y-m-d H:i:s') . ' +5 minutes')),
+                    ]);
+
+                    $this->otp->driver('email')->kirimOtp($email, $raw_token);
+            }
+        }
     }
 }
