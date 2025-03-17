66ae85a7a46c1207ef67b2a015dcf9f7e806b589
Agung Sugiarto <sugiartoagung92@gmail.com>
2025-03-17 11:10:23 +0700

    Penambahan Lampiran F101 - Malang

--- app/Libraries/TinyMCE.php
+++ app/Libraries/TinyMCE.php
@@ -733,8 +733,9 @@ class TinyMCE
             return;
         }
 
-        $surat  = $data['surat'];
-        $config = identitas();
+        $surat   = $data['surat'];
+        $config  = identitas();
+        $setting = setting();
 
         // TODO: Cek apakah ini masih digunakan, masih digunakan di lampiran surat
         $individu = (new PendudukSaja())->dataSurat($id);
