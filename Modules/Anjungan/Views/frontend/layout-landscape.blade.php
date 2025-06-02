fad16e9c4f1717fc8c16fa562333c9a1dcb897d5
Habibie <habibie.mahbub@gmail.com>
2025-06-02 16:00:22 +0700

    autoplay & mute

--- Modules/Anjungan/Views/frontend/layout-landscape.blade.php
+++ Modules/Anjungan/Views/frontend/layout-landscape.blade.php
@@ -149,9 +149,12 @@
                                 </div>
                             </div>
                         @else
-                            <div class="video-container">
-                                <iframe class="video-view" src="{{ setting('anjungan_youtube') }}?autoplay=1&controls=1&mute=0&loop=1" frameborder="0"></iframe>
-                            </div>
+                            <iframe class="video-view" 
+                                    src="{{ setting('anjungan_youtube') }}?autoplay=1&controls=1&mute=1&loop=1" 
+                                    frameborder="0" 
+                                    allow="autoplay; encrypted-media" 
+                                    allowfullscreen>
+                            </iframe>
                         @endif
 
                     </div>
