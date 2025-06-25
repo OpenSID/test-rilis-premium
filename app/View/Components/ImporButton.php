34490785ca7847f0adbaeaf6dbea5bdfc70fdc70
FirlianiF <firlianif@gmail.com>
2025-06-25 16:34:27 +0700

    update

--- app/View/Components/ImporButton.php
+++ app/View/Components/ImporButton.php
@@ -13,7 +13,9 @@ class ImporButton extends Component
      * Create a new component instance.
      */
     public function __construct(
-        public string $url
+        public string $url,
+        public string $judul = '',
+        public bool $modal = false
     ) {
     }
 
