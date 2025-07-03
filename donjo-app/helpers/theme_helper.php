8621ad45bf4bd43aee537ade93ebf6693e7a4fb6
Agung Sugiarto <sugiartoagung92@gmail.com>
2025-07-03 10:07:04 +0700

    fix: perbaiki tema tidak ditemukan

--- donjo-app/helpers/theme_helper.php
+++ donjo-app/helpers/theme_helper.php
@@ -94,8 +94,9 @@ if (! function_exists('theme_active')) {
             return theme()->aktif() ?? theme()->where('slug', Theme::DEFAULT_THEME)->first();
         });
 
-        // Catatan: Dipanggil disini karena di AppServiceProvider::register() belum bisa gunakan Elequent.
-        app('view')->addNamespace('theme', base_path($theme->view_path));
+        $viewPath = $theme->view_path ? $theme->view_path : theme()->aktif()->view_path;
+
+        view()->addNamespace('theme', base_path($viewPath));
 
         return $theme;
     }
