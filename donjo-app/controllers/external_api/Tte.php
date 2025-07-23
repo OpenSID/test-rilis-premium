115f118558934d2d92b571b5f9f757ee215ce11a
ahmad afandi <ahmad.afandi85@gmail.com>
2025-07-23 16:26:39 +0700

    update

--- donjo-app/controllers/external_api/Tte.php
+++ donjo-app/controllers/external_api/Tte.php
@@ -147,7 +147,7 @@ class Tte extends Tte_Controller
         } finally {
             // periksa apakah ada error pada response
             if ($errorMessage) {
-                return $this->logActivity('TTE', 'sign_visible', 'TTE Surat Gagal', [
+                return $this->logActivity('TTE', 'sign_invisible', 'TTE Surat Gagal', [
                     'id_surat'    => $data->id,
                     'no_surat'    => $data->no_surat,
                     'nama_surat'  => $data->nama_surat,
@@ -205,7 +205,6 @@ class Tte extends Tte_Controller
             ];
 
             $response = $this->client->post('api/sign/pdf', [
-                //'timeout'   => 6,
                 'headers'   => ['X-Requested-With' => 'XMLHttpRequest'],
                 'multipart' => [...$multipart, ...$visible],
             ]);
