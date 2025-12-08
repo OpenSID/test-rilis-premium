ae16b798b3df45229d4ae48ad6889351c0d8461e
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2025-12-08 15:17:24 +0800

    Update MigrationMakeCommand.php

--- app/Console/Commands/Modules/MigrationMakeCommand.php
+++ app/Console/Commands/Modules/MigrationMakeCommand.php
@@ -37,7 +37,7 @@
 
 namespace App\Console\Commands\Modules;
 
-use App\Console\Commands\Modules\BaseModuleMakeCommand;
+use Illuminate\Support\Str;
 
 class MigrationMakeCommand extends BaseModuleMakeCommand
 {
@@ -57,5 +57,45 @@ class MigrationMakeCommand extends BaseModuleMakeCommand
     protected function defaultNamespace(): string
     {
         return 'Database\\Migrations';
+    }
+
+    /**
+     * Dapatkan path lengkap file migrasi termasuk timestamp
+     */
+    protected function getDestinationFilePath(): string
+    {
+        $folder   = $this->getFolder();
+        $filename = $this->getFileName();
+
+        if (! is_dir($folder)) {
+            mkdir($folder, 0755, true);
+        }
+
+        return $folder . DIRECTORY_SEPARATOR . $filename;
+    }
+
+    /**
+     * Dapatkan nama file migrasi dengan timestamp
+     */
+    protected function getFileName(): string
+    {
+        $name      = $this->argument('name');
+        $timestamp = date('Y_m_d_His');
+
+        return $timestamp . '_' . Str::snake($name) . '.php';
+    }
+
+    /**
+     * Dapatkan folder tujuan migrasi
+     */
+    protected function getFolder(): string
+    {
+        $module = $this->option('module');
+
+        if ($module) {
+            return base_path("Modules/{$module}/{$this->moduleFolder()}");
+        }
+
+        return database_path('migrations');
     }
 }
