38a4c0c27fb2261528b607452a304d99bb389be8
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2026-01-28 21:19:38 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         /*
          * Package Service Providers...
          */
+        App\Providers\JsonApiPaginateServiceProvider::class,
 
         /*
          * Application Service Providers...
