df6d8dd0db21f26c2f8eb28317ae7dd970c92941
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2026-01-29 10:16:28 +0800

    ok

---
+++ laravel-ci3/src/Helpers/RedirectHelper.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * File ini bagian dari OpenSID
+ * Helper untuk Laravel redirect
+ */
+
+namespace OpenSID\LaravelCI3\Helpers;
+
+use Illuminate\Support\Facades\Redirect;
+
+class RedirectHelper
+{
+    /**
+     * Get the redirector instance atau redirect ke URL
+     * 
+     * @param string|null $location The URL or route to redirect to
+     * @param int $status HTTP status code
+     * @param array $headers Response headers
+     * @param bool|null $secure Use HTTPS
+     * @return Illuminate\Routing\Redirector|Illuminate\Http\RedirectResponse
+     */
+    public static function redirect($location = null, $status = 302, $headers = [], $secure = null)
+    {
+        // Jika location null, return redirector instance (untuk chainable: redirect()->to())
+        if (is_null($location)) {
+            return app('redirect');
+        }
+
+        // Convert relative path ke full URL jika bukan URL lengkap
+        if (! filter_var($location, FILTER_VALIDATE_URL)) {
+            $location = app('url')->to($location);
+        }
+
+        ci_redirect($location, 'location', $status);
+    }
+}
