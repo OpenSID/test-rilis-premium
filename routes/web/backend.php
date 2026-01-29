f9782987d0b0ec7068cfc0d0ab13ced8771e576b
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2026-01-29 14:54:32 +0800

    Update Ekspedisi.php

--- routes/web/backend.php
+++ routes/web/backend.php
@@ -38,27 +38,77 @@ use Illuminate\Support\Facades\Route;
  */
 
 // TODO:: Lebih baik buat migrasi untuk menghapus route yg lama
-Route::redirect('wilayah/clear', 'wilayah');
-Route::redirect('penduduk/clear', 'penduduk');
-Route::redirect('keluarga/clear', 'keluarga');
-Route::redirect('rtm/clear', 'rtm');
-Route::redirect('suplemen/clear', 'suplemen');
-Route::redirect('dokumen/clear', 'dokumen');
-Route::redirect('bumindes_umum', 'dokumen_sekretariat/peraturan');
-Route::redirect('ekspedisi/clear', 'ekspedisi');
-Route::redirect('bumindes_tanah_kas_desa/clear', 'inventaris_kekayaan');
-Route::redirect('dpt/clear', 'dpt');
-Route::redirect('statistik/clear', 'statistik');
-Route::redirect('klasifikasi/clear', 'klasifikasi');
-Route::redirect('bumindes_tanah_desa/clear', 'bumindes_tanah_desa');
-Route::redirect('bumindes_inventaris_kekayaan/clear', 'bumindes_inventaris_kekayaan');
-Route::redirect('bumindes_penduduk_induk/clear', 'bumindes_penduduk_induk');
-Route::redirect('bumindes_penduduk_rekapitulasi/clear', 'bumindes_penduduk_rekapitulasi');
-Route::redirect('bumindes_penduduk_ktpkk/clear', 'bumindes_penduduk_ktpkk');
-Route::redirect('modul/clear', 'modul');
-Route::redirect('qr_code', 'qrcode');
-Route::redirect('qrcode/clear', 'qrcode/clear');
-Route::redirect('web/clear', 'web');
-Route::redirect('slider/clear', 'slider');
-Route::redirect('web_widget/clear', 'web_widget');
-Route::redirect('pengunjung/clear', 'pengunjung');
+Route::get('wilayah/clear', function () {
+    redirect('wilayah');
+});
+Route::get('penduduk/clear', function () {
+    redirect('penduduk');
+});
+Route::get('keluarga/clear', function () {
+    redirect('keluarga');
+});
+Route::get('rtm/clear', function () {
+    redirect('rtm');
+});
+Route::get('suplemen/clear', function () {
+    redirect('suplemen');
+});
+Route::get('dokumen/clear', function () {
+    redirect('dokumen');
+});
+Route::get('bumindes_umum', function () {
+    redirect('dokumen_sekretariat/peraturan');
+});
+// Route::redirect('ekspedisi/clear', 'ekspedisi'); // Ini aneh, jadi double
+Route::get('ekspedisi/clear', function () {
+    redirect('ekspedisi');
+});
+Route::get('bumindes_tanah_kas_desa/clear', function () {
+    redirect('inventaris_kekayaan');
+});
+Route::get('dpt/clear', function () {
+    redirect('dpt');
+});
+// Route::redirect('statistik/clear', 'statistik'); // Ini aneh, jadi double
+Route::get('statistik/clear', function () {
+    redirect('statistik');
+});
+Route::get('klasifikasi/clear', function () {
+    redirect('klasifikasi');
+});
+Route::get('bumindes_tanah_desa/clear', function () {
+    redirect('bumindes_tanah_desa');
+});
+Route::get('bumindes_inventaris_kekayaan/clear', function () {
+    redirect('bumindes_inventaris_kekayaan');
+});
+Route::get('bumindes_penduduk_induk/clear', function () {
+    redirect('bumindes_penduduk_induk');
+});
+Route::get('bumindes_penduduk_rekapitulasi/clear', function () {
+    redirect('bumindes_penduduk_rekapitulasi');
+});
+Route::get('bumindes_penduduk_ktpkk/clear', function () {
+    redirect('bumindes_penduduk_ktpkk');
+});
+Route::get('modul/clear', function () {
+    redirect('modul');
+});
+Route::get('qr_code', function () {
+    redirect('qrcode');
+});
+Route::get('qrcode/clear', function () {
+    redirect('qrcode/clear');
+});
+Route::get('web/clear', function () {
+    redirect('web');
+});
+Route::get('slider/clear', function () {
+    redirect('slider');
+});
+Route::get('web_widget/clear', function () {
+    redirect('web_widget');
+});
+Route::get('pengunjung/clear', function () {
+    redirect('pengunjung');
+});
