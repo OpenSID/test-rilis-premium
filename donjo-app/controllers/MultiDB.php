d76a7793634443dd7fa3c8886913fb451d7cd5b2
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2026-02-18 09:11:38 +0800

    rilis: composer fix

--- donjo-app/controllers/MultiDB.php
+++ donjo-app/controllers/MultiDB.php
@@ -55,7 +55,7 @@ class MultiDB extends Admin_Controller
      * Daftar nama tabel yang hanya disertakan jika ada di database.
      */
     protected array $existenceTableNames = [
-        
+
     ];
 
     /**
@@ -265,7 +265,7 @@ class MultiDB extends Admin_Controller
      * dan TIDAK BOLEH diupdate manual
      */
     private array $skipPrimaryShift = [
-        'tweb_penduduk_mandiri'
+        'tweb_penduduk_mandiri',
     ];
 
     public function __construct()
